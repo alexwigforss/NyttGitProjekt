2e5e5b43217bdb04afa2adb8e56fc28b5b53c0f7
Alex Wigforss <alex.wigforss@gmail.com>
2023-02-22 14:04:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NyttGitProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Git!");
+        }
+    }
+}
